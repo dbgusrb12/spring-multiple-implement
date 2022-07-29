1b26a093a404e78a9178e6c04f74c779a970a3c0
hyungyu <yhg8484@naver.com>
2022-07-30 00:54:30 +0900

    [spring-multiple-implement] way8

---
+++ src/main/java/com/hg/way8/Way8Runner.java
@@ -0,0 +1,29 @@
+package com.hg.way8;
+
+import com.hg.service.SampleService;
+import com.hg.way8.SampleServiceAdapter.SampleServiceType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * way7 에서 구현체들의 관리를 따로 하는 객체를 생성해 위임한다.
+ */
+@Component
+@RequiredArgsConstructor
+public class Way8Runner implements ApplicationRunner {
+
+    private final SampleServiceAdapter sampleServiceAdapter;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        sampleServicePrint(SampleServiceType.FIRST);
+        sampleServicePrint(SampleServiceType.SECOND);
+    }
+
+    public void sampleServicePrint(SampleServiceType type) {
+        SampleService sampleService = sampleServiceAdapter.getSampleService(type);
+        sampleService.print();
+    }
+}
